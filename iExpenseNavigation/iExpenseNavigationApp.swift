a320132567d76b1adebde5730cb101828ae88d04
Henny2 <henrieke.baunack@web.de>
2023-11-19 18:19:30 -0800

    Initial Commit

---
+++ iExpenseNavigation/iExpenseNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseNavigationApp.swift
+//  iExpenseNavigation
+//
+//  Created by Henrieke Baunack on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
